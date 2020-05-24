8d3c97d59724d6138c5bd4e043bada8431aa4267
manishshrestha <manishshrestha2006@gmail.com>
2020-05-24 17:44:04 +0545

    Removed GameArea component. seems redundant

--- app/javascript/components/BooglePage.js
+++ app/javascript/components/BooglePage.js
@@ -1,14 +1,14 @@
 import React from "react"
 import GameIntro from "./section/GameIntro";
-import GameArea from "./section/GameArea";
 import ContactInfo from "./section/ContactInfo";
+import BoardArea from "./section/BoardArea";
 
 class BooglePage extends React.Component {
     render() {
         return (
             <div>
                 <GameIntro/>
-                <GameArea/>
+                <BoardArea/>
                 <ContactInfo/>
             </div>
         )
