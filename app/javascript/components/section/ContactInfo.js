79f5bdc731ded877cfb772d201e42ee672fecefc
manishshrestha <manishshrestha2006@gmail.com>
2020-05-24 17:48:51 +0545

    some minor adjustment in css and contact info

--- app/javascript/components/section/ContactInfo.js
+++ app/javascript/components/section/ContactInfo.js
@@ -4,7 +4,6 @@ class ContactInfo extends React.Component {
     render() {
         return (
             <div>
-                <br/>
                 <h3>Contact us</h3>
                 For any suggestions please contact Manish (manishshrestha2006 at gmail dot com).
             </div>
